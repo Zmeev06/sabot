6d32b817c409acb4c94779f4b297c1b0935fc4d7
woollywood <Gerbert.Wells969@yandex.ru>
2024-02-22 01:06:53 +0300

    position table fixed columns

--- src/components/positionTable/CellContentHeader.vue
+++ src/components/positionTable/CellContentHeader.vue
@@ -6,7 +6,7 @@
 </script>
 
 <template>
-	<div class="flex flex-col w-20 gap-1 items-start p-2">
+	<div class="flex flex-col w-full gap-1 items-start p-2">
 		<h2 class="text-text-primary text-sm/[14px] whitespace-nowrap">{{ date }}</h2>
 		<div class="text-fill-mid text-[12px] leading-3 whitespace-nowrap">{{ value }}</div>
 	</div>
--- src/modules/positionTable/components/Table.vue
+++ src/modules/positionTable/components/Table.vue
@@ -15,12 +15,16 @@
 	import { Pagination } from '@//components/pagination';
 	import RowExpanded from './RowExpanded.vue';
 
-	import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../../ui/table';
+	import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@//ui/table';
 
 	const props = defineProps<{
 		columns: ColumnDef<Response>[];
 		data: Response[];
 	}>();
+
+	const tableRef = ref();
+	const headerCell = ref();
+	const contentCell = ref();
 
 	const rowSelection = ref({});
 	const expanded = ref<ExpandedState>({});
@@ -47,6 +51,113 @@
 			},
 		},
 	});
+
+	function getCellWidth(index: number): number | string {
+		switch (index) {
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+				return 32;
+			case 4:
+				return 271;
+			case 5:
+				return 72;
+			default:
+				return '';
+		}
+	}
+
+	function isFixedCell(index: number) {
+		if (index > 5) {
+			return false;
+		}
+
+		return true;
+	}
+
+	function getFixedCellStyle(index: number) {
+		if (!isFixedCell(index)) {
+			return '';
+		}
+
+		return `sticky top-0 bg-base-white`;
+	}
+
+	/* */
+
+	function isHeaderRefExist(index: number) {
+		if (!headerCell.value) {
+			return false;
+		}
+
+		if (!headerCell.value[index]) {
+			return false;
+		}
+
+		return true;
+	}
+
+	function isHeaderFixedCell(index: number) {
+		if (!isHeaderRefExist(index)) {
+			return false;
+		}
+
+		if (!isFixedCell(index)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	function getFixedHeaderCellPosition(index: number) {
+		if (!isHeaderFixedCell(index)) {
+			return '';
+		}
+
+		const tableBox = tableRef.value?.$el.getBoundingClientRect();
+		const cellBox = headerCell.value[index].$el.getBoundingClientRect();
+
+		return cellBox.x - tableBox.x;
+	}
+
+	/* */
+
+	function isContentRefExits(rowIndex: number, cellIndex: number, rowLength: number) {
+		if (!contentCell.value) {
+			return false;
+		}
+
+		if (!contentCell.value[rowIndex * rowLength + cellIndex]) {
+			return false;
+		}
+
+		return true;
+	}
+
+	function isContentFixedCell(rowIndex: number, cellIndex: number, rowLength: number) {
+		if (!isContentRefExits(rowIndex, cellIndex, rowLength)) {
+			return false;
+		}
+
+		if (!isFixedCell(cellIndex)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	function getFixedContentCellPosition(rowIndex: number, cellIndex: number, rowLength: number) {
+		if (!isContentFixedCell(rowIndex, cellIndex, rowLength)) {
+			return '';
+		}
+
+		const currentIndex = rowIndex * rowLength + cellIndex;
+		const tableBox = tableRef.value?.$el.getBoundingClientRect();
+		const cellBox = contentCell.value[currentIndex].$el.getBoundingClientRect();
+
+		return cellBox.x - tableBox.x;
+	}
 </script>
 
 <template>
@@ -54,13 +165,22 @@
 		class="shadow-sm shadow-text-primary/5 outline outline-1 outline-border-mid rounded-xl overflow-hidden grid grid-flow-row">
 		<TableTopHeader class="border-b-[1px] border-b-border-heavy" />
 		<div class="w-full overflow-auto">
-			<Table>
+			<Table class="w-full table-auto" ref="tableRef">
 				<TableHeader class="!p-0 !border-none">
 					<TableRow
-						class="!p-0 border-b-[1px] border-b-border-heavy"
+						class="relative !p-0 border-b-[1px] border-b-border-heavy min-w-min max-w-min"
 						v-for="headerGroup in table.getHeaderGroups()"
 						:key="headerGroup.id">
-						<TableHead class="!p-0 !border-none" v-for="header in headerGroup.headers" :key="header.id">
+						<TableHead
+							class="!p-0 !border-none"
+							v-for="(header, index) in headerGroup.headers"
+							:key="header.id"
+							ref="headerCell"
+							:class="[getFixedCellStyle(index)]"
+							:style="{
+								width: `${getCellWidth(index)}px`,
+								left: `${getFixedHeaderCellPosition(index)}px`,
+							}">
 							<FlexRender
 								v-if="!header.isPlaceholder"
 								:render="header.column.columnDef.header"
@@ -71,17 +191,32 @@
 				<TableBody class="border-b-2">
 					<template v-if="table.getRowModel().rows?.length">
 						<TableRow
-							class="border-b-[1px] border-b-border-mid last:border-b-transparent transition-colors group"
-							:class="{
-								'hover:bg-grey-light hover:outline hover:outline-1 hover:outline-border-heavy':
-									row.depth === 0,
-								'outline outline-1 outline-border-heavy': row.getIsExpanded(),
-							}"
-							v-for="row in table.getRowModel().rows"
+							class="border-b-[1px] border-b-border-mid last:border-b-transparent transition-colors group min-w-min max-w-min"
+							:class="[
+								{
+									'hover:bg-grey-light hover:outline hover:outline-1 hover:outline-border-heavy':
+										row.depth === 0,
+									'outline outline-1 outline-border-heavy': row.getIsExpanded(),
+								},
+							]"
+							v-for="(row, rowIndex) in table.getRowModel().rows"
 							:key="row.id"
 							:data-state="row.getIsSelected() ? 'selected' : undefined">
 							<template v-if="row.depth === 0">
-								<TableCell class="h-[40px]" v-for="cell in row.getVisibleCells()" :key="cell.id">
+								<TableCell
+									class="h-[40px]"
+									v-for="(cell, cellIndex) in row.getVisibleCells()"
+									:key="cell.id"
+									ref="contentCell"
+									:class="[getFixedCellStyle(cellIndex)]"
+									:style="{
+										width: `${getCellWidth(cellIndex)}px`,
+										left: `${getFixedContentCellPosition(
+											rowIndex,
+											cellIndex,
+											table.getAllColumns().length
+										)}px`,
+									}">
 									<FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
 								</TableCell>
 							</template>
@@ -105,3 +240,9 @@
 		</div>
 	</div>
 </template>
+
+<style scoped>
+	.test {
+		@apply sticky left-0 top-0;
+	}
+</style>
