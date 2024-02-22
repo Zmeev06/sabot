8bfed81cb98337f8c0e71f21060381a7c7aae390
woollywood <Gerbert.Wells969@yandex.ru>
2024-02-22 03:04:12 +0300

    delete vue prime locale and added slider infographic

--- src/components/metricGroup/components/Infographic.vue
+++ src/components/metricGroup/components/Infographic.vue
@@ -2,6 +2,7 @@
 	import { Button } from '../../../ui/button';
 	import InfographicItem from './InfographicItem.vue';
 	import { IGraphicItem } from '../constants/types';
+	import { Swiper, SwiperSlide } from 'swiper/vue';
 
 	interface Props {
 		items: IGraphicItem[];
@@ -18,13 +19,14 @@
 
 <template>
 	<div class="grid grid-cols-[1fr_auto] items-center gap-2 w-full">
-		<div class="flex items-center rounded-lg overflow-hidden outline outline-1 outline-border-mid flex-wrap">
-			<InfographicItem
-				class="flex-auto outline outline-1 outline-border-mid"
-				v-for="(item, index) in items"
-				:key="index"
-				v-bind="item" />
-		</div>
+		<Swiper
+			class="relative w-full rounded-lg outline -outline-offset-1 outline-1 outline-border-mid after:absolute after:-right-2 after:top-0 after:h-full after:w-20 after:bg-gradient-to-r after:from-transparent after:to-base-white after:z-10 xl:after:content-none"
+			:slides-per-view="'auto'"
+			:resistanceRatio="0">
+			<SwiperSlide v-for="(item, index) in items" :key="index" class="!w-auto !flex-grow">
+				<InfographicItem class="-outline-offset-1 outline outline-1 outline-border-mid" v-bind="item" />
+			</SwiperSlide>
+		</Swiper>
 		<Button
 			icon="chevron-up"
 			class="!rounded-full transition-all"
